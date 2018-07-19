0719865fcc22eb871b4dd1f060b5a6f377ca9e11
Priscilla Okawa <prisoka@gmail.com>
2018-07-19 12:09:53 -0700

    fixed total cart syntax

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
       <div className="App">
         <CartHeader />
         <CartItems cartItemsList={cartItemsList} />
-        <p className="container" id='cartTotal'>Total: {this.state.cartTotal}</p>
+        <p className="container" id='cartTotal'>Total: ${this.state.cartTotal/100}</p>
         <AddItem products={products} onItemAdded={ (item) => this.addedItem(item)}/>
         <CartFooter />
         {/* <CartFooter copyright="whenever you're creating this app :)" /> */}
